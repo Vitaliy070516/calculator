9d90a67e81e14b220a324d54342027b51ff6d24d
goroshko <goroshko@rozetka.com.ua>
2022-03-14 19:28:27 +0200

    flutter web app

---
+++ build/precache-manifest.8671dfa4630783d9b3d9b4a2a51cc94a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5459885f0f8656e1111dc292a6a79dfb",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "11a111061e0acfb8c08e",
+    "url": "/calculator/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "11a111061e0acfb8c08e",
+    "url": "/calculator/static/js/main.7e8f5392.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
